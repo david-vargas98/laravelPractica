ad13fcaa0672a9be1d046b95241025a964d7a2b0
david-vargas98 <>
2023-09-07 16:53:28 -0600

    Se modificó el formulaio para reducirlo a una nueva versión de la base de datos.

--- resources/views/contacto.blade.php
+++ resources/views/contacto.blade.php
@@ -11,42 +11,21 @@
 <body>
     <header> <!-- Encabezado principal-->
         <h1>Contacto</h1>
+        <h3>
+            <?php echo $tipo; ?> <!-- También se puede hacer así: {{$tipo}}-->
+        </h3>
     </header>
     <main> <!-- Contenido principal-->
-        <form action="" method="post"> <!-- action se puede dejar en blanco o pasar el nombre del mismo archivo-->
+        <form action="validar-contacto" method="post"> <!-- action se puede dejar en blanco o pasar el nombre del mismo archivo-->
+            @csrf <!-- Crea un input oculto con u token, para saber que viene de la aplicación es para el POST xdd-->
             <label for="nombrePersona">Nombre:</label> <!-- for debe coincidir con id del input-->
             <input type="text" name="nombrePersona" id="nombrePersona" placeholder="Nombre completo" required> <br>
 
             <label for="correo">Correo:</label> <!-- for debe coincidir con id del input-->
-            <input type="email" name="correo" id="correo" placeholder="Correo electrónico" required> <br>
-
-            <label for="genero">Género:</label> <!-- for debe coincidir con id del input--> <br>
-            
-            <div class="genero">
-                <input type="radio" name="genero" id="masculino" value="masculino" required>
-                <label for="masculino">Masculino</label>
-                <input type="radio" name="genero" id="femenino" required>
-                <label for="femenino">Femenino</label>
-            </div>
-            <br>
-            <label for="contrasena">Contraseña:</label> <!-- for debe coincidir con id del input-->
-            <input type="password" name="contrasena" id="contrasena" placeholder="Contraseña" required> <br>
+            <input type="email" name="correo" id="correo" placeholder="Correo electrónico" required @if($tipo == 'alumno') value = "@alumnos.udg.mx" @else value = "@gmail.com" @endif> <br> <!--Se pasó variables-->
 
             <label for="comentarios">Comentarios:</label> <br> <!-- for debe coincidir con id del input-->
             <textarea name="comentarios" id="comentarios" cols="30" rows="10" placeholder="Escribe un comentario..."></textarea> <br>
-
-            <label for="ciudad">Ciudad:</label> <!-- for debe coincidir con id del input-->
-            <select name="ciudad" id="ciudad" required> <!-- etiqueta para despliegue de opciones-->
-                <option class="cursiva" value="" disabled selected>Seleccione una ciudad</option> <!-- Opciones en menú desplegable -->
-                <option value="Guadalajara">Guadalajara</option> <!-- Opciones en el menú desplegable -->
-                <option value="Tonala">Tonalá</option>
-                <option value="Zapopan">Zapopan</option>
-                <option value="Otro">Otro</option>
-            </select> <br>
-
-            <label for="">
-                <input type="checkbox" name="contratar" id="contratar">Me interesa contratarte
-            </label> <br>
 
             <button type="submit">Enviar formulario</button>
         </form>
